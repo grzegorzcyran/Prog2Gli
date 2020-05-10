843ce73c9b4e81f3c7f335686936876ed8a9f26a
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2020-05-10 13:51:17 +0200

    Memento pattern

---
+++ src/main/java/com/grcy/patterns/behavioral/memento/GameStateMemento.java
@@ -0,0 +1,19 @@
+package com.grcy.patterns.behavioral.memento;
+
+public class GameStateMemento {
+    private int id;
+    private String gameState;
+
+    public GameStateMemento(int id, String gameState) {
+        this.id = id;
+        this.gameState = gameState;
+    }
+
+    public String getGameState() {
+        return gameState;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
