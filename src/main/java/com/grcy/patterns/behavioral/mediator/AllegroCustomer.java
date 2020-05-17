a42c70c64c30e9aa5c6e8d0f0dde40b84a04266e
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2020-05-17 10:30:14 +0200

    Mediator pattern

---
+++ src/main/java/com/grcy/patterns/behavioral/mediator/AllegroCustomer.java
@@ -0,0 +1,18 @@
+package com.grcy.patterns.behavioral.mediator;
+
+public class AllegroCustomer extends Customer {
+
+    public AllegroCustomer(Mediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void bid(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public void cancelBid() {
+        this.price = -1;
+    }
+}
---
+++ src/main/java/com/grcy/patterns/behavioral/mediator/Mediator.java
@@ -0,0 +1,7 @@
+package com.grcy.patterns.behavioral.mediator;
+
+public interface Mediator {
+
+    void addBuyer(Customer customer);
+    void findTopBidder();
+}
